c5e3876023938ac27f6fa44676da89acbe477a75
neetibut <neetibut@gmail.com>
2024-01-24 18:07:37 +0700

    initial commit

---
+++ app/components/FormField.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FormFieldProps } from "@/types";
+
+const FormField: React.FC<FormFieldProps> = ({
+  type,
+  placeholder,
+  name,
+  register,
+  error,
+  valueAsNumber,
+}) => {
+  return (
+    <>
+      <input
+        type={type}
+        placeholder={placeholder}
+        {...register(name, { valueAsNumber })}
+      />
+      {error && <span className="error-message">{error.message}</span>}
+    </>
+  );
+};
+
+export default FormField;
